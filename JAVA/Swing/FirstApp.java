e36a7949b14732f57f59bae04471767773ee8618
DulangiAnuththara <43141556+DulangiAnuththara@users.noreply.github.com>
2020-03-05 11:18:14 +0530

    FirstJava Swing App

---
+++ JAVA/Swing/FirstApp.java
@@ -0,0 +1,18 @@
+//package Test.MainJava.com;
+import javax.swing.*;
+
+
+public class FirstApp{
+
+    public static void main(String[] args) {
+        
+        JFrame f = new JFrame();
+
+        JButton b = new JButton("Subscribe");
+        b.setBounds(140,100,120,40);
+        f.add(b);
+        f.setSize(400,500);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
